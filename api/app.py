fb7912de95da9b313981ac0e76ff285c136f72d2
liram11 <liram07@gmail.com>
2024-07-21 21:24:53 +0700

    api: summarize transcript

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
 from together import Together
@@ -17,15 +17,19 @@ app = Flask(__name__)
 def get_video_summary():
     video_id = request.args.get('video_id')
 
-    video_id = '3N4m5k9GAW0'
     full_transcript = download_full_transcript(video_id)
     comments = download_comments(video_id)
 
+    summary = summarize_transcript(full_transcript)
 
     return jsonify({
         "summary": summary,
         # "tr": full_transcript
     })
+
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return jsonify(error=str(e)), 500
 
 
 def download_full_transcript(video_id):
@@ -48,17 +52,47 @@ def download_comments(video_id):
     )
 
     response = request.execute()
-
-    print(response['items'][0]['snippet']['topLevelComment']['snippet']['likeCount'])
-
     comments = []
 
     for item in response['items']:
         comment = item['snippet']['topLevelComment']['snippet']
         comments.append([comment['likeCount'], comment['textDisplay']])
 
-
     return comments
+
+def summarize_transcript(transcript):
+    response = AiClient.chat.completions.create(
+        model="meta-llama/Meta-Llama-3-70B-Instruct-Turbo",
+        max_tokens=512,
+        temperature=0.2,
+        top_p=0.7,
+        top_k=50,
+        repetition_penalty=1,
+        messages=[{
+            "role": "user",
+            "content": '''
+                Generate a summary for a YouTube video transcript. Important: Summary length must not exceed 15 words.
+
+                You must respond ONLY with the JSON schema with the following structure. Do not add any additional comments. Don't make any greetings, like, "Here your response".
+
+                You NEVER use " only '.
+
+                Return me a valid json in this format:
+                {
+                    "summary": <Summary goes here>,
+                }
+
+                Here is the transcript:
+            '''  + transcript
+        }],
+    )
+
+    print(response.choices[0].message.content)
+
+    data = json.loads(response.choices[0].message.content)
+
+    return data
+
 
 
 if __name__ == '__main__':
