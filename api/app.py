202325efd9a9a18decda6ba78014d7ad8c090080
liram11 <liram07@gmail.com>
2024-07-21 23:24:19 +0700

    api: get comments clickbait mentions

--- api/app.py
+++ api/app.py
@@ -21,9 +21,12 @@ def get_video_summary():
     comments = download_comments(video_id)
     video_info = download_video_info(video_id)
 
-    print(video_info)
+    summary = process_transcript(full_transcript)
+    comment_info = process_comments(comments)
 
-    summary = summarize_transcript(full_transcript)
+    print(comment_info)
+
+
 
     return jsonify({
         "summary": summary,
@@ -35,6 +38,7 @@ def get_video_summary():
 #     return jsonify(error=str(e)), 500
 
 
+# ================== Fetch data from YouTube ==================
 def download_full_transcript(video_id):
     transcripts = YouTubeTranscriptApi.get_transcript(video_id)
 
@@ -77,7 +81,10 @@ def download_video_info(video_id):
         "like_count": response['items'][0]['statistics']['likeCount'],
     }
 
-def summarize_transcript(transcript):
+# =============================================================
+# ======================= Process Data ========================
+
+def process_transcript(transcript):
     response = AiClient.chat.completions.create(
         model="meta-llama/Meta-Llama-3-70B-Instruct-Turbo",
         max_tokens=512,
@@ -88,29 +95,90 @@ def summarize_transcript(transcript):
         messages=[{
             "role": "user",
             "content": '''
-                Generate a summary for a YouTube video transcript. Important: Summary length must not exceed 15 words.
+                Generate a summary for a YouTube video transcript. Important: Summary length must not exceed 15 words. Those symbols are not allowed in the summary text: ".
 
                 You must respond ONLY with the JSON schema with the following structure. Do not add any additional comments. Don't make any greetings, like, "Here your response".
 
-                You NEVER use " only '.
-
                 Return me a valid json in this format:
                 {
-                    "summary": <Summary goes here>,
+                    "summary": "<Summary goes here>",
                 }
 
                 Here is the transcript:
-            '''  + transcript
+            ''' + transcript
         }],
     )
 
     print(response.choices[0].message.content)
-
     data = json.loads(response.choices[0].message.content)
 
     return data
 
+def process_comments(comments):
+    if len(comments) == 0:
+        return {
+            "comments_summary": "Not enough comments",
+            "clickbait_mentions": 0
+        }
 
+    comments_text = ''
+    for comment in comments:
+        if len(comments_text) + len(comment[1]) < 7000:
+            comments_text += "[likes_count: {count}]\n".format(count=comment[1])
+            comments_text += comment[1] + '\n'
+
+    response = AiClient.chat.completions.create(
+        model="meta-llama/Meta-Llama-3-70B-Instruct-Turbo",
+        max_tokens=512,
+        temperature=0.2,
+        top_p=0.7,
+        top_k=50,
+        repetition_penalty=1,
+        messages=[{
+            "role": "user",
+            "content": '''
+                I will provide you with a list of comments from a YouTube video. I will provide you comment likes amount and comment text.
+
+                I want you to provide a summary of the most discussed things. Make sure to focus on most liked comments.
+
+                I also want you to count amount mentions of 'clickbait' in a comments.
+
+                I will provide comments in the following format:
+                ```
+                    [likes_count: number]
+                    comment_text
+                    [likes_count: number]
+                    comment_text
+                    ...
+                ```
+
+                Important: Summary length must not exceed 20 words. Those symbols are not allowed in the summary text: ".
+
+                Do not add any additional comments. Don't make any greetings, like, "Here your response".
+
+                You must respond ONLY with the JSON schema with the following structure.
+
+                Return me a valid json in this format:
+                {
+                    "comments_summary": "<Summary goes here>",
+                    "clickbait_mentions": <Clickbait count goes here>,
+                }
+
+                Here are the comments:
+            ''' + comments_text
+        }],
+    )
+
+    print ('comments')
+    print(response.choices[0].message.content)
+    data = json.loads(response.choices[0].message.content)
+
+    return data
+# =============================================================
+# ======================== Calc score =========================
+
+
+# =============================================================
 
 if __name__ == '__main__':
     app.run(debug=True)
